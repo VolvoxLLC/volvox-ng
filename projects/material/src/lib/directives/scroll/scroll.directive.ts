7c8b16a962f87444f772bc99d6fa9e4003bcb41c
Samuel <samifmoutinho@gmail.com>
2021-02-17 10:53:23 +0100

    fix tslint

--- projects/material/src/lib/directives/scroll/scroll.directive.ts
+++ projects/material/src/lib/directives/scroll/scroll.directive.ts
@@ -5,13 +5,14 @@ import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 })
 export class ScrollDirective {
 
+    // tslint:disable-next-line:no-output-on-prefix
     @Output()
     public onScroll: EventEmitter<Event> = new EventEmitter<Event>();
 
     constructor() {
     }
 
-    @HostListener('window:scroll', ['$event'])
+    @HostListener('window:scroll', [ '$event' ])
     private scroll(e: Event): void {
         this.onScroll.emit(e);
     }
