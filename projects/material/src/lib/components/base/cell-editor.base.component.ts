73ea6e4140374cc392e1ec463a91c22dc3d6e1e1
Samuel <samifmoutinho@gmail.com>
2021-05-02 15:08:19 +0200

    cell editor

--- projects/material/src/lib/components/base/cell-editor.base.component.ts
+++ projects/material/src/lib/components/base/cell-editor.base.component.ts
@@ -6,7 +6,6 @@ import { PromiseReject, PromiseResolve } from '@volvox-ng/core';
 import { Observable } from 'rxjs';
 import { ConfirmDialogData, ConfirmDialogResult } from '../../models/confirm-dialog.model';
 import { TableItem } from '../../models/table-item.model';
-import { CellEditorBaseFacade } from '../../services/facades/cell-editor-base.facade';
 import { ConfirmDialog } from '../confirm-dialog/confirm.dialog';
 import { BaseComponent } from './base.component';
 
@@ -26,14 +25,14 @@ export class CellEditorBaseComponent<Model> extends BaseComponent implements OnI
         super();
     }
 
-    public get changedRows(): TableItem<Model>[] {
-        return this.dataSource?.data?.filter((c: TableItem<Model>): boolean => c.rowChangedRowKeys?.length > 0);
-    }
-
     public ngOnInit(): void {
         super.ngOnInit();
         this.dataSource = new MatTableDataSource<TableItem<Model>>();
         this.displayedColumns = [];
+    }
+
+    public get changedRows(): TableItem<Model>[] {
+        return this.dataSource?.data?.filter((c: TableItem<Model>): boolean => c.changedRowKeys?.length > 0);
     }
 
     public canDeactivate(component: CellEditorBaseComponent<Model>, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot,
@@ -46,7 +45,8 @@ export class CellEditorBaseComponent<Model> extends BaseComponent implements OnI
             this.myMatDialog
                 .open(ConfirmDialog, {
                     width: '600px',
-                    data: new ConfirmDialogData('', ''),
+                    data: new ConfirmDialogData('volvox.components.cellEditorBase.discardChanges.label',
+                        'volvox.components.cellEditorBase.discardChanges.message'),
                 })
                 .afterClosed()
                 .subscribe((result: ConfirmDialogResult): void => {
--- projects/material/src/lib/models/table-item.model.ts
+++ projects/material/src/lib/models/table-item.model.ts
@@ -1,5 +1,5 @@
 ﻿export interface ITableItem<T> {
-    rowChangedRowKeys: string[];
+    changedRowKeys: string[];
     rowOriginalData: T;
 }
 
