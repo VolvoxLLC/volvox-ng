57b604847cc8dbfa561aa03f57e03f2698617870
Samuel <samifmoutinho@gmail.com>
2021-05-19 20:27:47 +0200

    loading button spinner

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -1,1 +1,70 @@
-export * from './index';
+/*
+ * Public API Surface of core
+ */
+
+/**
+ * Services
+ */
+export * from './lib/services/api.service';
+export * from './lib/services/volvox-socket.service';
+export * from './lib/services/volvox-translate.service';
+export * from './lib/services/core-logger.service';
+
+/**
+ * Guards
+ */
+export * from './lib/services/guards/can-deactivate.guard';
+
+/**
+ * Components
+ */
+export * from './lib/volvox.module';
+
+/**
+ * Models
+ */
+export * from './lib/models/dictionary-item.model';
+export * from './lib/models/api-options.model';
+export * from './lib/models/volvox-duration.model';
+export * from './lib/models/logger-config.model';
+export * from './lib/models/token-settings.model';
+export * from './lib/models/transform-matrix.model';
+export * from './lib/models/sieve-response.model';
+
+/**
+ * Utils
+ */
+export * from './lib/utils/dictionary.util';
+export * from './lib/utils/prototypes.util';
+export * from './lib/utils/sieve.util';
+export * from './lib/utils/size.util';
+export * from './lib/utils/promise.util';
+export * from './lib/utils/item.util';
+export * from './lib/utils/commons.util';
+
+/**
+ * Enums
+ */
+export * from './lib/enums/theme.enum';
+export * from './lib/enums/content-position.enum';
+export * from './lib/enums/browser.enum';
+export * from './lib/enums/i18n-supported.enum';
+
+/**
+ * Pipes
+ */
+export * from './lib/pipes/volvox-date-pipe/volvox-date-pipe.module';
+export * from './lib/pipes/volvox-date-pipe/volvox-date.pipe';
+
+export * from './lib/pipes/dictionary-item-pipe/dictionary-item-pipe.module';
+export * from './lib/pipes/dictionary-item-pipe/dictionary-item.pipe';
+
+export * from './lib/pipes/volvox-duration-pipe/volvox-duration-pipe.module';
+export * from './lib/pipes/volvox-duration-pipe/volvox-duration.pipe';
+
+export * from './lib/pipes/find-in-array/find-in-array-pipe.module';
+export * from './lib/pipes/find-in-array/find-in-array.pipe';
+
+export * from './lib/pipes/join/join-pipe.module';
+export * from './lib/pipes/join/join.pipe';
+
